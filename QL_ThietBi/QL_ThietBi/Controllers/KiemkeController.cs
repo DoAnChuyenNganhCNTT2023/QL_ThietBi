0ff1a7e4c05ccc80a192bd58e6eba01bc5496eac
Thao <106434043+thaokawaii@users.noreply.github.com>
2023-11-10 15:10:38 +0700

    10/11

--- QL_ThietBi/QL_ThietBi/Controllers/KiemkeController.cs
+++ QL_ThietBi/QL_ThietBi/Controllers/KiemkeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using QL_ThietBi.Models;
+using PagedList;
 
 namespace QL_ThietBi.Controllers
 {
@@ -15,14 +16,53 @@ namespace QL_ThietBi.Controllers
         {
             return View();
         }
-        public ActionResult DS_ThietBi()
+        public ActionResult DS_ThietBi(int ?page)
         {
-            var ds = from item in dt.THIETBIs select item;
-            return View(ds.ToList());
+          
+            if (page == null) page = 1;
+
+            var links = (from item in dt.THIETBIs select item).OrderBy(x => x.MALOAI);
+
+
+            int pageSize = 10;
+
+            int pageNumber = (page ?? 1);
+
+     
+            return View(links.ToPagedList(pageNumber, pageSize));
         }
 
-        [HttpGet]
-        public ActionResult KiemKe()
+       
+        public ActionResult KiemKe(int ?page)
+        {
+
+            if (page == null) page = 1;
+
+            var links = (from item in dt.PHIEUGHINHANHUHONGs select item).OrderBy(x => x.MANV);
+
+
+            int pageSize = 10;
+
+            int pageNumber = (page ?? 1);
+
+
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult CT_DSHuHong(int ?page,string ID)
+        {
+            if (page == null) page = 1;
+
+            var links = (from item in dt.CTPHIEUHHs where item.ID_PHIEUGNHH==ID select item).OrderBy(x => x.MATB);
+
+
+            int pageSize = 10;
+
+            int pageNumber = (page ?? 1);
+            ViewBag.Ma = ID;
+
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult Add_PhieuHH()
         {
             return View();
         }
