1509974bc49ad96d1ea20793d8161f25ec99a766
Thao <106434043+thaokawaii@users.noreply.github.com>
2023-11-10 07:55:58 +0700

    10/11

---
+++ QL_ThietBi/QL_ThietBi/Controllers/KiemkeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QL_ThietBi.Models;
+
+namespace QL_ThietBi.Controllers
+{
+    public class KiemkeController : Controller
+    {
+        // GET: Kiemke
+        QL_THIETBIDataContext dt = new QL_THIETBIDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DS_ThietBi()
+        {
+            var ds = from item in dt.THIETBIs select item;
+            return View(ds.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult KiemKe()
+        {
+            return View();
+        }
+    }
+}
