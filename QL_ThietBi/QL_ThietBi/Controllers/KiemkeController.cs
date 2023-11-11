7cdd187c1c224e2a8fede237e7da189c128fd2be
Thao <106434043+thaokawaii@users.noreply.github.com>
2023-11-11 15:37:54 +0700

    update

--- QL_ThietBi/QL_ThietBi/Controllers/KiemkeController.cs
+++ QL_ThietBi/QL_ThietBi/Controllers/KiemkeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using QL_ThietBi.Models;
 using PagedList;
+using System.Web.UI;
+using System.Data.SqlClient;
 
 namespace QL_ThietBi.Controllers
 {
@@ -59,6 +61,10 @@ namespace QL_ThietBi.Controllers
 
             int pageNumber = (page ?? 1);
             ViewBag.Ma = ID;
+            var ds = from d in dt.THIETBIs select d;
+            List<THIETBI> products = ds.ToList<THIETBI>();
+            ViewBag.ThietBi = new SelectList(products, "MATB", "TENTB");
+          
 
             return View(links.ToPagedList(pageNumber, pageSize));
         }
@@ -75,5 +81,104 @@ namespace QL_ThietBi.Controllers
 
             return RedirectToAction("KiemKe");
         }
+      
+        public ActionResult Add_CTHH(string ID,string thietbi,string soluong,string ghichu)
+        {
+            CTPHIEUHH p = (from item in dt.CTPHIEUHHs where item.MATB == thietbi && item.ID_PHIEUGNHH == ID select item).FirstOrDefault();
+            CTPHIEUHH ph = new CTPHIEUHH();
+            if (p == null)
+            {
+                try
+                {
+                    ph.MATB = thietbi;
+                    ph.SOLUONG = int.Parse(soluong);
+                    ph.ID_PHIEUGNHH = ID;
+                    ph.GHICHU = ghichu;
+                    dt.CTPHIEUHHs.InsertOnSubmit(ph);
+                    dt.SubmitChanges();
+                }
+                catch (SqlException ex)
+                {
+                    // Bắt lỗi từ SQL Server
+                    foreach (SqlError error in ex.Errors)
+                    {
+                        if (error.Class == 16) // Class 16 thường là lỗi do người dùng
+                        {
+                            // Xử lý lỗi trigger
+                            string errorMessage = error.Message;
+
+                            // Hiển thị thông báo lỗi cho người dùng sử dụng mã JavaScript
+                            string script = $"alert('{errorMessage}');";
+                       //     ScriptManager.RegisterStartupScript(HttpContext.Current.Handler as Page, typeof(Page), "ServerControlScript", script, true);
+
+                            // Hoặc thực hiện các hành động khác
+                            // ...
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Bắt lỗi chung
+                    // Xử lý lỗi hoặc hiển thị thông báo cho người dùng
+                    string errorMessage = ex.Message;
+                    string script = $"alert('{errorMessage}');";
+                  //  ScriptManager.RegisterStartupScript(HttpContext.Current.Handler as Page, typeof(Page), "ServerControlScript", script, true);
+                    // ...
+                }
+            }
+            else
+            {
+                p.SOLUONG += int.Parse(soluong);
+               
+                dt.SubmitChanges();
+            }
+            
+            
+                return RedirectToAction("CT_DSHuHong", new { ID });
+            
+         
+
+        }
+        public ActionResult Delete_CTHH(string id,string thietbi)
+        {
+            CTPHIEUHH ph = (from item in dt.CTPHIEUHHs where item.MATB == thietbi && item.ID_PHIEUGNHH == id select item).FirstOrDefault();
+            dt.CTPHIEUHHs.DeleteOnSubmit(ph);
+            dt.SubmitChanges();
+            return RedirectToAction("CT_DSHuHong", new { id });
+        }
+        public ActionResult Update_CTHH(string id,string thietbi,string soluong,string submitButton,string ghichu)
+        {
+            CTPHIEUHH ph = (from item in dt.CTPHIEUHHs where item.MATB == thietbi && item.ID_PHIEUGNHH == id select item).FirstOrDefault();
+            //ph.SOLUONG = int.Parse(soluong);
+            //dt.SubmitChanges();
+            if (submitButton == "Update")
+            {
+                // Xử lý cập nhật nhân viên
+                
+                if (ph != null)
+                {
+                   ph.SOLUONG = int.Parse(soluong);
+                    ph.GHICHU = ghichu;
+                    dt.SubmitChanges();
+                  
+                }
+            }
+            else if (submitButton == "Delete")
+            {
+                // Xử lý xóa nhân viên
+          
+                if (ph != null)
+                {
+                    dt.CTPHIEUHHs.DeleteOnSubmit(ph);
+                    dt.SubmitChanges();
+                }
+            }
+            return RedirectToAction("CT_DSHuHong", new { id });
+        }
+        public ActionResult ThanhLy()
+        {
+            return View();
+        }
+        
     }
 }
